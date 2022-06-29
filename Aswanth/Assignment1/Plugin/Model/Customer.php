7022bcb09c15c50f3a1d230056b5a5acb14ba555
Your Name <aswanth.k@codilar.com>
2022-06-29 20:11:19 +0530

    error fix

--- Aswanth/Assignment1/Plugin/Model/Customer.php
+++ Aswanth/Assignment1/Plugin/Model/Customer.php
@@ -1,25 +1,50 @@
 <?php
+
 namespace Aswanth\Assignment1\Plugin\Model;
+
 use Magento\Framework\Stdlib\DateTime\DateTime;
+
 class Customer
 {
-
+    /**
+     * Date
+     *
+     * @var DateTime
+     */
     private DateTime $dateTime;
 
+    /**
+     * Construct
+     *
+     * @param DateTime $dateTime
+     */
     public function __construct(DateTime $dateTime)
     {
         $this->dateTime=$dateTime;
     }
 
+    /**
+     * Fetching Dob
+     *
+     * @param \Magento\Customer\Model\Data\Customer $subject
+     * @param mixed $result
+     * @return float
+     */
     public function afterGetMiddlename(
         \Magento\Customer\Model\Data\Customer $subject,
         $result
     ) {
-       $dob=$subject->getDob();
-       $result=$this->calculateAge($dob);
-       return $result;
+        $dob=$subject->getDob();
+        $result=$this->calculateAge($dob);
+        return $result;
     }
 
+    /**
+     * Calculate age
+     *
+     * @param string $dob
+     * @return floatS
+     */
     public function calculateAge($dob)
     {
         $bday =$this->dateTime->gmtTimestamp($dob);
